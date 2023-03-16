b1d512da855333c03025d91a33cd3ca83ffb43cd
haraldjm <haraldjm@gmail.com>
2023-03-16 10:26:23 +0100

    PESB-1 Refactor paquetes y clases

--- src/test/java/com/eoi/springboot/MySpringBootApplicationTests.java
+++ src/test/java/com/eoi/springboot/MySpringBootApplicationTests.java
@@ -1,13 +1,59 @@
 package com.eoi.springboot;
 
+import lombok.extern.log4j.Log4j2;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+
+ * The type Demo application tests.
+
+ */
+@SpringBootTest(classes = MySpringBootApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Log4j2
 class MySpringBootApplicationTests {
 
+	@Autowired
+	private TestRestTemplate template;
+
+	/**
+
+	 * Hello main.
+
+	 *
+
+	 * @throws Exception the exception
+
+	 */
 	@Test
-	void contextLoads() {
+	void hello_main() throws Exception {
+		ResponseEntity<String> response = template.getForEntity("/", String.class);
+		assertThat(response.getBody()).isEqualTo("Main class");
+	}
+
+	/**
+
+	 * Hello hola.
+
+	 *
+
+	 * @throws Exception the exception
+
+	 */
+	@Test
+	void hello_hola() throws Exception {
+		ResponseEntity<String> response = template.getForEntity("/hola", String.class);
+		assertThat(response.getBody()).isEqualTo("Hola mundo!3");
 	}
 
 }
